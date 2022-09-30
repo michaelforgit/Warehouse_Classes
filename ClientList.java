74f6de8c4b93edfb3fd94d1d4055851ab3a616a4
Romeo Mouk <71787723+Romeeeo@users.noreply.github.com>
2022-09-29 23:52:02 -0500

    Add files via upload

---
+++ ClientList.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+public class ClientList {
+  private LinkedList<Client> clients;	
+
+  public ClientList() {
+	clients = new LinkedList<>();
+  }
+  
+  public boolean insertClient (Client client) {
+    clients.add(client);
+    return true;
+  }
+  
+  public void displayClients (PrintWriter p) {
+	Iterator i = clients.iterator();
+	while(i.hasNext()){
+		p.println(i.next().toString());
+	}
+  }
+  
+  public int findClient (String cid) {
+	 int i = 0;
+	 for (Iterator it = clients.iterator(); it.hasNext(); i++) {
+			Client client = (Client) it.next();
+			 if (client.getId().equals(cid)) {
+				 return i;
+			 }
+		 }
+		 return -1;
+	 }
+
+}
